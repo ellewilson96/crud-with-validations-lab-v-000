e4e396eeca450c010dc28f2bf43d496ce975b9b3
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-11 15:28:21 +0000

    automatically backed up by learn

--- db/migrate/20180711152726_create_songs.rb
+++ db/migrate/20180711152726_create_songs.rb
@@ -1,10 +1,10 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-      t.string, :title
-      t.boolean, :released
-      t.integer, :release_year
-      t.string, :artist_name
+      t.string :title
+      t.boolean :released
+      t.integer :release_year
+      t.string :artist_name
       t.string :genre
 
       t.timestamps null: false
