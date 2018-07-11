5eb9f04cfb6ad9ad59a63f9b552dc312b97d33d2
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-11 15:33:03 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,10 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :title, uniqueness: true
   validates :released, inclusion: {in: %w(true false)}
-  if :released.valid? = true 
-  validates :release_year, presence: true 
-  else 
-  validates :release_year, presence: false 
-  end 
+  if :released.valid? = true
+  validates :release_year, presence: true
+  else
+  validates :release_year, presence: false
+  end
 
 end
