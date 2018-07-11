44beae2f448274a753bd20a89866dd7733b7d771
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-11 15:38:48 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,24 +1,25 @@
 class SongsController < ApplicationController
-  def show 
+  def show
     @song = Song.find(params[:id])
-  end 
+  end
 
-  def new 
-    @song = Song.new 
-  end 
+  def new
+    @song = Song.new
+  end
 
-  def create 
+  def create
     @song = Song.new(song_params)
-  if @song.save 
+  if @song.save
     redirect_to song_path(@song)
-  else 
-    render :new 
-  end 
+  else
+    render :new
+  end
 end
 
   private
 
-  def song_params 
+  def song_params
     params.permit(:title, :released, :artist_name, :genre)
-  
+  end
+
 end
