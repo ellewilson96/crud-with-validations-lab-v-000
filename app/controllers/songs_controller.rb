4ff75a3f75200b16474e80d13f80ff68e8c53a0b
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-11 15:38:44 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,24 @@
 class SongsController < ApplicationController
+  def show 
+    @song = Song.find(params[:id])
+  end 
+
+  def new 
+    @song = Song.new 
+  end 
+
+  def create 
+    @song = Song.new(song_params)
+  if @song.save 
+    redirect_to song_path(@song)
+  else 
+    render :new 
+  end 
+end
+
+  private
+
+  def song_params 
+    params.permit(:title, :released, :artist_name, :genre)
+  
 end
